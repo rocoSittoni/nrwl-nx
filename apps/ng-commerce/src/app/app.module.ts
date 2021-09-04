7c443be9bdbbaeb237c5be756661740dbbc75d8b
rocoSittoni <roco.sittoni@protonmail.com>
2021-09-04 18:50:42 -0300

    first commit, project setup

--- apps/ng-commerce/src/app/app.module.ts
+++ apps/ng-commerce/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routing-module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductsListComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
