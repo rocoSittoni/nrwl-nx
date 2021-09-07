4f5f21ad4bca4732977c21b5987d68e8652a2ba9
rocoSittoni <roco.sittoni@protonmail.com>
2021-09-07 18:22:56 -0300

    sidebar created with its routes for the dashboardpages

--- apps/ng-dashboard/src/app/app.module.ts
+++ apps/ng-dashboard/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent],
-    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, ProductsComponent, CategoriesComponent, UsersComponent],
+    imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent]
 })
