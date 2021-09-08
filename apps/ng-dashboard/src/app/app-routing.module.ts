5bd95fae0353f25d08d2d3a7c5000775c16183f3
rocoSittoni <roco.sittoni@protonmail.com>
2021-09-08 12:23:52 -0300

    sidebar style finished and orders route added

--- apps/ng-dashboard/src/app/app-routing.module.ts
+++ apps/ng-dashboard/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { UsersComponent } from './pages/users/users.component';
 import { ShellComponent } from './shared/shell/shell.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'categories', component: CategoriesComponent },
+      { path: 'orders', component: OrdersComponent },
       { path: 'users', component: UsersComponent }
     ]
   },
--- apps/ng-dashboard/src/app/app.module.ts
+++ apps/ng-dashboard/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '../../../../libs/ui/src/lib/material.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -9,10 +10,24 @@ import { ShellComponent } from './shared/shell/shell.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { UsersComponent } from './pages/users/users.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, ProductsComponent, CategoriesComponent, UsersComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        ShellComponent,
+        SidebarComponent,
+        ProductsComponent,
+        CategoriesComponent,
+        UsersComponent,
+        OrdersComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MaterialModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
