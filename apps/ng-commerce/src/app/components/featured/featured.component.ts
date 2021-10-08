f5eaa16f71d69c04270cdd02e80dacfe855cdcd6
roco sittoni <roco.sittoni@protonmail.com>
2021-10-08 18:40:59 -0300

    featured products ready

--- apps/ng-commerce/src/app/components/featured/featured.component.ts
+++ apps/ng-commerce/src/app/components/featured/featured.component.ts
@@ -54,13 +54,13 @@ export class FeaturedComponent implements OnInit, OnDestroy {
   breakpoints = {
     300: { slidesPerView: 1, spaceBetween: 10 },
     640: { slidesPerView: 2, spaceBetween: 20 },
-    768: { slidesPerView: 4, spaceBetween: 40 },
+    768: { slidesPerView: 3, spaceBetween: 50 },
     1024: { slidesPerView: 4, spaceBetween: 40 },
   };
 
   config: SwiperOptions = {
-    slidesPerView: 3,
-    spaceBetween: 50,
+    slidesPerView: 5,
+    spaceBetween: 20,
     navigation: true,
     virtual: true,
     centeredSlides: true,
